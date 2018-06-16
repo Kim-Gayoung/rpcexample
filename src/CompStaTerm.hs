34449e75f13277de2d740b15e3e529655055ebbc
kwanghoon <lazyswamp@gmail.com>
2018-06-16 11:54:46 +0900

    Added a little piece of code to CompStaTerm

--- src/CompStaTerm.hs
+++ src/CompStaTerm.hs
@@ -1,5 +1,22 @@
 module CompStaTerm where
 
--- To be implemented soon!!
+import Term(Location(..))
+import TypedTerm as TT 
+import StaTerm as ST 
+
+-- Compilation for Client
+compStaTerm :: TypedTerm -> StaTerm
+compStaTerm m = ST.Const 0
+
+
+compClient :: Int -> TypedTerm -> (Int, StaTerm) 
+compClient i m = (i, ST.Const 0)
+
+
+-- Compilation for Server
+compServer :: Int -> TypedTerm -> (Int, StaTerm)
+compServer i m = (i, ST.Const 0)
+
+
 
 
